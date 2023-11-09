b05c4060fe82ee791407bf31210077d999c20785
pius_desiderius <miroshnikovaa@my.msu.ru>
2023-11-09 16:23:08 +0200

    pic builder

--- picture_builder.py
+++ picture_builder.py
@@ -1,20 +1,9 @@
 import os.path as op
-import os
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
-import pandas as pd
 from itertools import compress
 import time
 import logging
-
 import mne
-import mne_nirs
-from mne.preprocessing.nirs import optical_density, beer_lambert_law
-
-from mne.preprocessing.nirs import (optical_density,
-                                    temporal_derivative_distribution_repair,
-                                    scalp_coupling_index)
 from mne import events_from_annotations
 
 from functions_fnirs import *
@@ -68,8 +57,6 @@ for filename in recordings_names[0:2]:
                             tmax=TMAX, 
                             baseline=BASELINE 
                             )
-
-    
     #reassign channels to pick
     picks_hbo_left, picks_hbr_left = C3_chans_of_interest_hbo, C3_chans_of_interest_hbr
     picks_hbo_right, picks_hbr_right = C4_chans_of_interest_hbo, C4_chans_of_interest_hbr
@@ -80,61 +67,88 @@ for filename in recordings_names[0:2]:
     rest_epochs_left = rest_epochs.copy().pick_channels(picks_hbo_left + picks_hbr_left)
     rest_epochs_right = rest_epochs.copy().pick_channels(picks_hbo_right + picks_hbr_right)
 
-    # evoked_dict_left = {f'{CONDITION}/HbO': smr_epochs_left.copy().average(picks=picks_hbo_left),
-    #             f'{CONDITION}/HbR': smr_epochs_left.copy().average(picks=picks_hbr_left),
-    #             'Rest/HbO': rest_epochs_left.copy().average(picks=picks_hbo_left),
-    #             'Rest/HbR': rest_epochs_left.copy().average(picks=picks_hbr_left)}
+    evoked_dict_left = {f'{CONDITION}/HbO': smr_epochs_left.copy().average(picks=picks_hbo_left),
+                f'{CONDITION}/HbR': smr_epochs_left.copy().average(picks=picks_hbr_left),
+                'Rest/HbO': rest_epochs_left.copy().average(picks=picks_hbo_left),
+                'Rest/HbR': rest_epochs_left.copy().average(picks=picks_hbr_left)}
 
-    # evoked_dict_right = {f'{CONDITION}/HbO': smr_epochs_right.copy().average(picks=picks_hbo_right),
-    #             f'{CONDITION}/HbR': smr_epochs_right.copy().average(picks=picks_hbr_right),
-    #             'Rest/HbO': rest_epochs_right.copy().average(picks=picks_hbo_right),
-    #             'Rest/HbR': rest_epochs_right.copy().average(picks=picks_hbr_right)}
+    evoked_dict_right = {f'{CONDITION}/HbO': smr_epochs_right.copy().average(picks=picks_hbo_right),
+                f'{CONDITION}/HbR': smr_epochs_right.copy().average(picks=picks_hbr_right),
+                'Rest/HbO': rest_epochs_right.copy().average(picks=picks_hbo_right),
+                'Rest/HbR': rest_epochs_right.copy().average(picks=picks_hbr_right)}
     
-    evoked_dict_left = {f'{CONDITION}/HbO': smr_epochs_left.copy().get_data().mean(axis=[0, 1]),
-                f'{CONDITION}/HbR': smr_epochs_left.copy().get_data().mean(axis=[0, 1]),
-                'Rest/HbO': rest_epochs_left.copy().get_data().mean(axis=[0, 1]),
-                'Rest/HbR': rest_epochs_left.copy().get_data().mean(axis=[0, 1])}
+    # evoked_dict_left = {f'{CONDITION}/HbO': smr_epochs_left.copy().get_data().mean(axis=[0, 1]),
+    #             f'{CONDITION}/HbR': smr_epochs_left.copy().get_data().mean(axis=[0, 1]),
+    #             'Rest/HbO': rest_epochs_left.copy().get_data().mean(axis=[0, 1]),
+    #             'Rest/HbR': rest_epochs_left.copy().get_data().mean(axis=[0, 1])}
 
-    evoked_dict_right = {f'{CONDITION}/HbO': smr_epochs_right.copy().get_data().mean(axis=[0, 1]),
-                f'{CONDITION}/HbR': smr_epochs_right.copy().get_data().mean(axis=[0, 1]),
-                'Rest/HbO': rest_epochs_right.copy().get_data().mean(axis=[0, 1]),
-                'Rest/HbR': rest_epochs_right.copy().get_data().mean(axis=[0, 1])}
+    # evoked_dict_right = {f'{CONDITION}/HbO': smr_epochs_right.copy().get_data().mean(axis=[0, 1]),
+    #             f'{CONDITION}/HbR': smr_epochs_right.copy().get_data().mean(axis=[0, 1]),
+    #             'Rest/HbO': rest_epochs_right.copy().get_data().mean(axis=[0, 1]),
+    #             'Rest/HbR': rest_epochs_right.copy().get_data().mean(axis=[0, 1])}
     
 
-    # a_right = evoked_dict_right[f'{CONDITION}/HbO'].get_data()
-    # b_right = evoked_dict_right[f'{CONDITION}/HbR'].get_data()
-    # c_right = evoked_dict_right['Rest/HbO'].get_data()
-    # d_right = evoked_dict_right['Rest/HbR'].get_data()
-    # right_np_array = np.mean(np.stack(arrays=[a_right, b_right, c_right, d_right]), axis=1).reshape(4, 1, a_right.shape[1])
-    # np.save(rf'{dirs_to_save_stuff["haemo_folder_path_np"]}\{SUBJECT} {CONDITION} right.npy', right_np_array)
+    target_right_hbo = evoked_dict_right[f'{CONDITION}/HbO'].get_data()
+    target_right_hbr = evoked_dict_right[f'{CONDITION}/HbR'].get_data()
+    target_right_hbt = hbt_total(target_right_hbr, target_right_hbo)
+    target_right_oxy = oxy_level(target_right_hbo, target_right_hbr)
     
-    # a_left= evoked_dict_left[f'{CONDITION}/HbO'].get_data()
-    # b_left= evoked_dict_left[f'{CONDITION}/HbR'].get_data()
-    # c_left= evoked_dict_left['Rest/HbO'].get_data()
-    # d_left= evoked_dict_left['Rest/HbR'].get_data()
-    # left_np_array = np.mean(np.stack(arrays=[a_left, b_left, c_left, d_left]), axis=1).reshape(4, 1, a_left.shape[1])
-    # np.save(rf'{dirs_to_save_stuff["haemo_folder_path_np"]}\{SUBJECT} {CONDITION} left.npy', left_np_array)
+    rest_right_hbo = evoked_dict_right['Rest/HbO'].get_data()
+    rest_right_hbr = evoked_dict_right['Rest/HbR'].get_data()
+    rest_right_hbt = hbt_total(rest_right_hbr, rest_right_hbo)
+    rest_right_oxy = oxy_level(rest_right_hbo, rest_right_hbr)
+    
+    right_arrays = {'hbo': [target_right_hbo, rest_right_hbo],
+                    'hbr':[target_right_hbr, rest_right_hbr],
+                    'hbt':[target_right_hbt, rest_right_hbt],
+                    'oxy':[target_right_oxy, rest_right_oxy]  
+    }
+    
+    right_np_array = np.mean(np.stack(arrays=[a_right, b_right, c_right, d_right]), axis=1).reshape(4, 1, a_right.shape[1])
+    npy_path_right = op.join(DIRS_TO_SAVE_STUFF, f'{SUBJECT} {CONDITION} right.npy')
+    np.save(npy_path_right, right_np_array)
+    
+    target_left_hbo = evoked_dict_right[f'{CONDITION}/HbO'].get_data()
+    target_left_hbr = evoked_dict_right[f'{CONDITION}/HbR'].get_data()
+    target_left_hbt = hbt_total(target_left_hbr, target_left_hbo)
+    target_left_oxy = oxy_level(target_left_hbo, target_left_hbr)
+    
+    rest_left_hbo = evoked_dict_right['Rest/HbO'].get_data()
+    rest_left_hbr = evoked_dict_right['Rest/HbR'].get_data()
+    rest_left_hbt = hbt_total(rest_left_hbr, rest_left_hbo)
+    rest_left_oxy = oxy_level(rest_left_hbo, rest_left_hbr)
+    
+        
+    left_arrays = {'hbo': [target_left_hbo, rest_left_hbo],
+                    'hbr':[target_left_hbr, rest_left_hbr],
+                    'hbt':[target_left_hbt, rest_left_hbt],
+                    'oxy':[target_left_oxy, rest_left_oxy]  
+    }
+    
+    left_np_array = np.mean(np.stack(arrays=[a_left, b_left, c_left, d_left]), axis=1).reshape(4, 1, a_left.shape[1])
+    npy_path_left = op.join(DIRS_TO_SAVE_STUFF, f'{SUBJECT} {CONDITION} left.npy')
+    np.save(npy_path_right, left_np_array)
 
 
-    # for condition in evoked_dict_left:
-    #     evoked_dict_left[condition].rename_channels(lambda x: x[:-4])
-    # for condition in evoked_dict_right:
-    #     evoked_dict_right[condition].rename_channels(lambda x: x[:-4])
+    for condition in evoked_dict_left:
+        evoked_dict_left[condition].rename_channels(lambda x: x[:-4])
+    for condition in evoked_dict_right:
+        evoked_dict_right[condition].rename_channels(lambda x: x[:-4])
 
-    # fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    fig, axes = plt.subplots(1, 2, figsize=(20, 12))
 
-    # color_dict = dict(HbO='#AA3377', HbR='b')
-    # styles_dict = dict(Rest=dict(linestyle='dashed'))
+    color_dict = dict(HbO='#AA3377', HbR='b')
+    styles_dict = dict(Rest=dict(linestyle='dashed'))
 
-    # #this is made of scales unification
-    # y_min = min(evoked_dict_left[f'{CONDITION}/HbO'].get_data().min(),
-    #             evoked_dict_right[f'{CONDITION}/HbO'].get_data().min(),()) * 1.5*10**6
-    # y_max = max(evoked_dict_left[f'{CONDITION}/HbO'].get_data().max(),
-    #             evoked_dict_right[f'{CONDITION}/HbO'].get_data().max(),()) * 1.5*10**6
-    # ylim = {'hbo':[y_min, y_max],
-    #         'hbr':[y_min, y_max]}
+    #this is made of scales unification
+    y_min = min(evoked_dict_left[f'{CONDITION}/HbO'].get_data().min(),
+                evoked_dict_right[f'{CONDITION}/HbO'].get_data().min(),()) * 1.5*10**6
+    y_max = max(evoked_dict_left[f'{CONDITION}/HbO'].get_data().max(),
+                evoked_dict_right[f'{CONDITION}/HbO'].get_data().max(),()) * 1.5*10**6
+    ylim = {'hbo':[y_min, y_max],
+            'hbr':[y_min, y_max]}
 
-    # mne.viz.plot_compare_evokeds(evoked_dict_left,
+    mne.viz.plot_compare_evokeds(evoked_dict_left,
     #                                         combine="mean",
     #                                         ci=0.95,
     #                                         colors=color_dict,
